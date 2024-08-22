yaap/vendor_yaap

[SQUASH] Merge LineageOS LatinIME dictionaries
Squashed commit of the following:

commit f5e55a1da751a29e909319a4a65676d124347309
Author: John Galt <johngaltfirstrun@gmail.com>
Date:   Mon Jul 22 10:09:51 2024 -0400

    overlay: move and include dictionaries on vanilla

commit 8fff04186732a4eb81513914b35c78f2a4c5b2eb
Author: Eloi Carracedo <eloimuns@gmail.com>
Date:   Tue Jun 2 08:06:59 2020 +0200

    Update Bulgarian dictionary

    Sourced from: https://github.com/dslul/openboard/pull/108

    A total of approximately 750 words and their forms were added. The first line was amended with the new version and a correction for the name of the dictionary, which did not list the language name with a capitalized first letter as it should have.

    The newly added words are mostly names of local dishes (cuisine), old or dialect words, Turkish loanwords that are commonplace, curses and offensive words and words pertaining to gender studies.
    As is noted in the original source (https://github.com/dslul/openboard/issues/105), the new lines received the addition of ',flags=originalFreq=' for future proofing, but the old lines did not.

    Other things to note if somebody wishes to improve this in the future:
    1. The general frequency is skewed and needs to be heavily improved.
    2. Offensive and possibly offensive flags are missing, as are any other flags that may exist.
    3. Common personal and family names, famous historical figures' names, as well as monikers and/or celebrities' pseudonyms could be added to further enrich the dictionary.
    4. The prefixes 'по' and 'най' (hyphenated in front of certain words) can't be handled by the dictionary as is.

    Change-Id: Iee6d2e43166b324ec16f5bbe00dceac8b9289b67

commit e1b3a5ad10e09677c9d7dc332e5fe45073b2fb6d
Author: Micha? Trzebiatowski <michtrz@gmail.com>
Date:   Thu Aug 15 23:53:49 2019 +0200

    LatinIME: Add en_GB, en_US, lt, lv, ro, sl, sr, tr dictionaries

    Missing dictionaries were built from wordlis

config/common.mk
@@ -89,6 +89,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/yaap/overlay
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/yaap/overlay
 ifneq ($(TARGET_BUILD_GAPPS),true)
 PRODUCT_PACKAGE_OVERLAYS += vendor/yaap/overlay-vanilla
+# Include LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/yaap/overlay-vanilla/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/yaap/overlay-vanilla/dictionaries
 endif
 PRODUCT_COPY_FILES += \
     vendor/yaap/overlay/partition_order.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/partition_order.xml

NAAP-again/vendor_naap

config/common.mk
@@ -106,6 +106,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/naap/overlay
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/naap/overlay
 ifneq ($(TARGET_BUILD_GAPPS),true)
 PRODUCT_PACKAGE_OVERLAYS += vendor/naap/overlay-vanilla
+# Include LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/naap/overlay-vanilla/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/naap/overlay-vanilla/dictionaries
 endif
 PRODUCT_COPY_FILES += \
     vendor/naap/overlay/partition_order.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/partition_order.xml